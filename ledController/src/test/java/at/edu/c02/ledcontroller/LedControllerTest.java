aeceaff50f2a94258c02dd7b6f631e29dd63e143
Markus Stoff <s52005@edu.campus02.at>
2024-01-27 14:11:12 +0100

    Implement tests for getGroupLeds() and bug fixes
    Closes #2

--- ledController/src/test/java/at/edu/c02/ledcontroller/LedControllerTest.java
+++ ledController/src/test/java/at/edu/c02/ledcontroller/LedControllerTest.java
@@ -1,8 +1,13 @@
 package at.edu.c02.ledcontroller;
 
+import org.json.JSONObject;
 import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
 
 public class LedControllerTest {
@@ -13,5 +18,28 @@ public class LedControllerTest {
     @Test
     public void dummyTest() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void getGroupLedsRequestsStatusOfAllLights() throws IOException {
+        ApiService service = mock(ApiService.class);
+        when(service.getLights()).thenReturn(new JSONObject("{\"lights\":[{\"on\":false,\"id\":5,\"color\":\"#fff\",\"groupByGroup\":{\"name\":\"F\"}}]}"));
+
+        LedController controller = new LedControllerImpl(service);
+        controller.getGroupLeds();
+
+        verify(service).getLights();
+        verifyNoMoreInteractions(service);
+    }
+
+    @Test
+    public void getGroupLedsOnlyReturnsLedsOfGroupG() throws IOException {
+        ApiService service = mock(ApiService.class);
+        when(service.getLights()).thenReturn(new JSONObject("{\"lights\":[{\"on\":false,\"id\":5,\"color\":\"#fff\",\"groupByGroup\":{\"name\":\"F\"}},{\"on\":false,\"id\":6,\"color\":\"#fff\",\"groupByGroup\":{\"name\":\"G\"}}]}"));
+
+        LedController controller = new LedControllerImpl(service);
+        LedStatus[] groupLeds = controller.getGroupLeds();
+
+        assertEquals(1, groupLeds.length);
     }
 }
